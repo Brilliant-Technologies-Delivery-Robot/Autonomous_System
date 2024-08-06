35fe47d7921af5c21e84e58a36d465028c027b26
Abdelraman24 <bodydia78@gmail.com>
2024-08-06 17:38:48 +0300

    Automated goal execution script

---
+++ integration/integration/scripts/automated_goal_execution.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Point, Quaternion
+
+def move_to_goal(x, y, z, qx, qy, qz, qw):
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position = Point(x, y, z)
+    goal.target_pose.pose.orientation = Quaternion(qx, qy, qz, qw)
+
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('send_goals_py')
+
+        goals = [
+            (1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0),
+            (2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0),
+            (3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0)
+        ]
+
+        for goal in goals:
+            result = move_to_goal(*goal)
+            if result:
+                rospy.loginfo("Goal execution done!")
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation test finished.")
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Point, Quaternion
+
+def move_to_goal(x, y, z, qx, qy, qz, qw):
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position = Point(x, y, z)
+    goal.target_pose.pose.orientation = Quaternion(qx, qy, qz, qw)
+
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('send_goals_py')
+
+        goals = [
+            (1.229, 2.619, 0.0, 0.0, 0.0, 0.636, 0.77166),
+            (1.8, 5.38, 0.0, 0.0, 0.0, -0.7, 0.618),
+            (0.72, 0.42, 0.0, 0.0, 0.0, 0.6568, 0.754),
+            (-1.099, 1.0679, 0.0, 0.0, 0.0, 0.99127, 0.1317)
+        ]
+
+        for goal in goals:
+            result = move_to_goal(*goal)
+            if result:
+                rospy.loginfo("Goal execution done!")
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation test finished.")
