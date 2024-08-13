5cd6b8cecc569e61d2d070b581aa81070f08c745
ALAAUSAMA9 <110568739+ALAAUSAMA9@users.noreply.github.com>
2024-08-13 20:34:29 +0300

    add new maps

--- integration/delivery_robot_urdf2/scripts/normalization.py
+++ integration/delivery_robot_urdf2/scripts/normalization.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 def normalize_quaternion(quaternion):
     x = quaternion.x
@@ -25,14 +26,21 @@ def normalize_quaternion(quaternion):
 def imu_callback(data):
     normalized_quaternion = normalize_quaternion(data.orientation)
     data.orientation = normalized_quaternion
-    data.orientation_covariance = [0.1, 0, 0, 0, 0.1, 0, 0, 0, 0.1]
-    print(data.orientation.x, data.orientation.y, euler_from_quaternion([data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])[2])
+    data.orientation_covariance = [0.01, 0, 0, 0, 0.01, 0, 0, 0, 0.01]
+    # print(data.orientation.x, data.orientation.y, euler_from_quaternion([data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])[2])
     imu_pub.publish(data)
+
+def odom_callback(data):
+    # print odom orientation data in euler angles
+    orientation = data.pose.pose.orientation
+    euler_angles = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
+    print(euler_angles)
 
 while not rospy.is_shutdown():
     rospy.init_node('imu_normalizer', anonymous=True)
 
     imu_sub = rospy.Subscriber('/imu/data', Imu, imu_callback)
     imu_pub = rospy.Publisher('/imu_data', Imu, queue_size=10)
+    odom_sub = rospy.Subscriber('/robot_pose_ekf/odom_combined', PoseWithCovarianceStamped, odom_callback)
 
     rospy.spin()
