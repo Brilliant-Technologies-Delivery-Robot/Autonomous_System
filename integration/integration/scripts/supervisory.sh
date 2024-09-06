a77049eccdc3ae745e74d5af4fe144dce18843f8
OmarRamzy45 <ramzyomar0@gmail.com>
2024-09-06 14:39:30 +0300

    update supervisory node

--- integration/integration/scripts/supervisory.sh
+++ integration/integration/scripts/supervisory.sh
@@ -16,7 +16,6 @@ check_errors() {
         sleep 10
         return 1
     fi
-
     # Check if imu is connected
     if ! ls /dev/ttyUSB1 &>/dev/null; then
         echo "IMU not connected or cannot find port, please reconnect IMU. If the problem persists, check the USB port number"
@@ -27,6 +26,13 @@ check_errors() {
     # Check if arduino is connected
     if ! ls /dev/ttyACM0 &>/dev/null; then
         echo "Arduino not connected or cannot find port, please reconnect Arduino. If the problem persists, check the USB port number"
+        echo "system will restart in 10 seconds"
+        sleep 10
+        return 1
+    fi
+    # Check if /position topic is being published
+    if ! rostopic list | grep -q "/position"; then
+        echo "/position topic not being published, please check the rosserial node"
         echo "system will restart in 10 seconds"
         sleep 10
         return 1
